aadd7e9cbbff50bf8f49bbf0c3514973259036e5
xissburg <xissburg@gmail.com>
2012-01-28 00:29:08 -0200

    Added S4Lucene with as less shit as possible. Fuck this library.

--- LuceneTest/ViewController.h
+++ LuceneTest/ViewController.h
@@ -8,6 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+
+@property (strong, nonatomic) NSMutableArray *searchResultsArray;
 
 @end
--- LuceneTest/ViewController.m
+++ LuceneTest/ViewController.m
@@ -7,13 +7,21 @@
 //
 
 #import "ViewController.h"
+#import "OHAttributedLabel.h"
+#import "NSAttributedString+Attributes.h"
+
+#define kOHAttributedLabelTag 123456
+#define kSearchResultArrayKey @"searchResultArray"
+#define kFileNameKey @"fileName"
 
 @implementation ViewController
+@synthesize tableView = _tableView;
+@synthesize searchBar = _searchBar;
+@synthesize searchResultsArray;
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Release any cached data, images, etc that aren't in use.
 }
 
 #pragma mark - View lifecycle
@@ -21,40 +29,79 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.searchResultsArray = [[NSMutableArray alloc] init];
 }
 
 - (void)viewDidUnload
 {
+    [self setTableView:nil];
+    [self setSearchBar:nil];
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
 }
 
-- (void)viewWillAppear:(BOOL)animated
+- (void) viewWillAppear:(BOOL)animated
 {
+    [self.navigationController setNavigationBarHidden:YES animated:animated];
     [super viewWillAppear:animated];
 }
 
-- (void)viewDidAppear:(BOOL)animated
+- (void) viewWillDisappear:(BOOL)animated
 {
-    [super viewDidAppear:animated];
-}
-
-- (void)viewWillDisappear:(BOOL)animated
-{
-	[super viewWillDisappear:animated];
-}
-
-- (void)viewDidDisappear:(BOOL)animated
-{
-	[super viewDidDisappear:animated];
+    [self.navigationController setNavigationBarHidden:NO animated:animated];
+    [super viewWillDisappear:animated];
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     // Return YES for supported orientations
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return [self.searchResultsArray count];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    NSDictionary *fileSearchDictionary = [self.searchResultsArray objectAtIndex:section];
+    NSArray *fileSearchArray = [fileSearchDictionary objectForKey:kSearchResultArrayKey];
+    return [fileSearchArray count];
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    NSDictionary *fileSearchDictionary = [self.searchResultsArray objectAtIndex:section];
+    return [fileSearchDictionary objectForKey:kFileNameKey];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    OHAttributedLabel *label = (OHAttributedLabel *)[cell viewWithTag:kOHAttributedLabelTag];
+    label.attributedText = [self.searchResultsArray objectAtIndex:indexPath.row];
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate 
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+#pragma mark - UISearchBarDelegate
+
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText
+{
+    
+}
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar
+{
+    [searchBar resignFirstResponder];
 }
 
 @end
